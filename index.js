572da6d8cf5baae184a80f89a935119b79e4ed39
caleigh <caleighsteill@outlook.com>
2022-12-18 12:57:41 -0700

    commit tree 2945374fc6b8fbcbb64f28893212d76fe19bf74d

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(response => response.json())
+//     .then(formData => console.log(formData))
+//     .catch(error => {
+//         alert("Bad things!")
+//         console.log(error.message)
+//     })
+
+
+function submitData(name, email) {
+    return fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+        .then(response => response.json())
+        .then(userData => document.body.innerHTML = userData.id)
+        .catch(error => document.body.innerHTML = error.message)
+}
